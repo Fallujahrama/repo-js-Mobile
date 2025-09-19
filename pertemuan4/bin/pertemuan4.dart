647fe80c60a56e7b9d107befdeefc4dd190bb451
Fallujahrama <fallujahrc@gmail.com>
2025-09-19 13:34:14 +0700

    set, nilai uniq

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -3,33 +3,55 @@ import 'dart:io';
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  // print('Hello world: ${pertemuan4.calculate()}!');
+  // // print('Hello world: ${pertemuan4.calculate()}!');
 
-  // array fix length
-  var fixList = List<int>.filled(4, 13);
-  fixList[0] = 1;
-  fixList[1] = 2;
-  fixList[2] = 3;
-  fixList[3] = 4;
-  // fixList[4] = 5;
+  // // array fix length
+  // var fixList = List<int>.filled(4, 13);
+  // fixList[0] = 1;
+  // fixList[1] = 2;
+  // fixList[2] = 3;
+  // fixList[3] = 4;
+  // // fixList[4] = 5;
 
-  // print isi list
-  // print(fixList);
-  stdout.writeln(fixList);
-  // stdout.writeln(fixList[1]);
+  // // print isi list
+  // // print(fixList);
+  // stdout.writeln(fixList);
+  // // stdout.writeln(fixList[1]);
 
-  var fl = List<String>.filled(3, '');
-  fl[0] = "satu";
-  fl[1] = "dua";
-  fl[2] = "tiga";
-  stdout.writeln(fl);
+  // var fl = List<String>.filled(3, '');
+  // fl[0] = "satu";
+  // fl[1] = "dua";
+  // fl[2] = "tiga";
+  // stdout.writeln(fl);
 
-  var growList = [3, 4, 5];
-  growList.add(1);
-  growList.add(2);
-  growList.add(3);
-  growList.remove(2);
+  // var growList = [3, 4, 5];
+  // growList.add(1);
+  // growList.add(2);
+  // growList.add(3);
+  // growList.remove(2);
 
-  stdout.writeln(growList);
+  // stdout.writeln(growList);
 
+  var setNilai1 = <int>{1, 2, 3, 4};
+  var setNilai2 = <int>{1, 1, 2, 2, 5};
+  
+  // // print(setNilai1);
+  // print(setNilai2);
+  // // gabungan 2 set
+  // print(setNilai1.union(setNilai2));
+  // // // irisan 2 set
+  // print(setNilai1.intersection(setNilai2));
+
+  var setNilai3 = <int>{};
+
+  stdout.write("Jumlah data setNilai3 = ");
+  String? input = stdin.readLineSync();
+  int jumlah1 = int.tryParse(input ?? '') ?? 0;
+  for (int i = 0; i < jumlah1; i++) {
+    stdout.write("Masukkan nilai ke-${i + 1} = ");
+    String? nilaiInput = stdin.readLineSync();
+    int nilai = int.tryParse(nilaiInput ?? '') ?? 0;
+    setNilai3.add(nilai);
+  }
+  print(setNilai3);
 }
