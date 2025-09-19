dfb519aea5b974d08340ded35bde2914760936e1
Fallujahrama <fallujahrc@gmail.com>
2025-09-19 11:21:27 +0700

    fixed list

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -1,5 +1,26 @@
+import 'dart:io';
+
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan4.calculate()}!');
+  // print('Hello world: ${pertemuan4.calculate()}!');
+
+  // array fix length
+  var fixList = List<int>.filled(4, 13);
+  fixList[0] = 1;
+  fixList[1] = 2;
+  fixList[2] = 3;
+  fixList[3] = 4;
+  // fixList[4] = 5;
+
+  // print isi list
+  // print(fixList);
+  stdout.writeln(fixList);
+  // stdout.writeln(fixList[1]);
+
+  var fl = List<String>.filled(3, '');
+  fl[0] = "satu";
+  fl[1] = "dua";
+  fl[2] = "tiga";
+  stdout.writeln(fl);
 }
